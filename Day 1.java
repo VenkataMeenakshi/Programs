1455b59ebeddd8f9370ef8a16b76efb12b1ecab5
VenkataMeenakshi <pathaguntameenakshi@gmail.com>
2025-08-19 17:16:33 +0530

    Create Day 1.java

---
+++ Day 1.java
@@ -0,0 +1,66 @@
+public class Variables {
+	public static void main(String[] args)
+	 {
+		String name = "Saravanan";int age = 25;               float percent = 89.9f;
+		char gender = 'M'; 
+		  booleanmarried = false;
+		  System.out.println("Name :" + name);
+		System.out.println("Age : " + age);
+		System.out.println("percent : " + percent);          System.out.println("Gender : " + gender);             System.out.println("Married : " + married);
+}
+}
+
+TYPE CASTING
+
+class casting 
+{
+	public static void main(String[] args) {
+		double d = 25.5385;
+	int c = (int)d;
+	System.out.println("Double :" + d);
+	System.out.println("Int :" + c);
+	}
+}
+
+
+class casting
+ {
+ 	public static void main(String[] args)
+ 	 {
+ 	 	int a = 10;
+ 	 	double b = a;
+ 	 	System.out.println("Int :" + a);
+ 	 	System.out.println("Double :" + b);
+ 	 	}
+ }
+
+
+ARTHIMETIC OPERATOR
+
+class add 
+{
+	public static void main(String[] args)
+	 {
+	 	int a = 123, b = 10;
+	 	System.out.println("Addition : " + (a + b));          System.out.println("Subtraction : " + (a - b));        System.out.println("Multiplication : " + (a *b));   
+	 	System.out.println("Division : " + (a / b));              System.out.println("Modulo : " + (a % b));
+	 	}
+	}
+
+ASSIGNMENT OPERATOR
+
+
+class Assignment
+ {
+ 	public static void main(String[] args)
+ 	 {
+ 	 	int a = 123;
+ 	 	System.out.println(a); 
+ 	 	System.out.println(a); 
+ 	 	System.out.println(a);  
+ 	 	System.out.println(a); 
+ 	 	System.out.println(a); 
+ 	 	System.out.println(a); 
+ 	 	}
+ 	 
+ 	 }
